e9136416c2675ae9edb257a8f7b04084b81c4776
Danmark Mutai <danmark.chemuren@gmail.com>
2019-05-13 18:10:16 +0300

    Added javascript file

---
+++ js/styles.js
@@ -0,0 +1,17 @@
+
+
+function randomNumber() {
+    return Math.ceil(Math.random()*6);
+}
+function rollDice1() {
+    var placeholder1 = document.getElementById("placeholder1");
+    placeholder1.innerHTML = randomNumber();
+}
+
+function randomNumber() {
+    return Math.ceil(Math.random()*6);
+}
+function rollDice2() {
+    var placeholder2 = document.getElementById("placeholder2");
+    placeholder2.innerHTML = randomNumber();
+}
