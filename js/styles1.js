f7729eb1f39607efa048cc6bbb409eff93e0b2c7
Danmark Mutai <danmark.chemuren@gmail.com>
2019-05-14 12:17:05 +0300

    Removed some insertions in javascript,the style1 file

--- js/styles1.js
+++ js/styles1.js
@@ -4,49 +4,4 @@ $(document).ready(function () {
         $(".showRules").toggle('fast');
         $(".hideRules").toggle('fast');
     });
-    $('#rollDice1').click(function (event) { //roll button for player1
-        event.preventDefault();
-        player1.active = true;
-        player2.active = false;
-        player1.roll(); //call the function to generate random numbers
-        $('.diceRoll1').text(player1.diceRoll); //display the rolled dice number
-        $('.turnScore1').text(player1.turnTotal); //display the turn score (temporary score)
-    });
-    $('.roll2').click(function (event) { //roll button for player2
-        event.preventDefault();
-        //Activate Gaming Area
-        player2.active = true;
-        player1.active = false;
-        player2.roll(); //call the function to generate random numbers
-        $('.diceRoll2').text(player2.diceRoll); //display the rolled dice number
-        $('.turnScore2').text(player2.turnTotal); //display the turn score (temporary score)
-    });
-    //Display overall score when the hold button is clicked
-    $('#holdDice1').click(function (event) { //hold button for player1
-        event.preventDefault();
-        //Deactivate Gaming Area
-        player1.active = false;
-        player2.active = true;
-        player1.hold(); //call the function to add the turn score to the overall score
-        $('.overallScore1').text(player1.overallScore); //display the overall score
-        
-        player1.diceRoll = 0;
-        player1.turnTotal = 0;
-        $('.diceRoll1').text(player1.diceRoll);
-        $('.turnScore1').text(player1.turnTotal);
-    });
-    //hold button for player2
-    $('#holdDice2').click(function (event) { 
-        event.preventDefault();
-       
-        player2.active = false;
-        player1.active = true;
-        player2.hold(); //call the function to add the turn score to the overall score
-        $('.overallScore2').text(player2.overallScore); //display the overall score
-        //Clear turn score and total score
-        player2.diceRoll = 0;
-        player2.turnTotal = 0;
-        $('.diceRoll2').text(player2.diceRoll);
-        $('.turnScore2').text(player2.turnTotal);
-    });
 });
